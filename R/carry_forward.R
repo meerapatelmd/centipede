2046d99a8263db10aab38e37a746b1019e58fc78
Meera Patel <patelm9@mskcc.org>
2019-10-03 02:03:38 -0400

    +: functions

--- R/carry_forward.R
+++ R/carry_forward.R
@@ -1,10 +1,12 @@
-
-
+#' Imputes true NAs with the prior value in a character string (excluding position 1)
+#' @param vector character vector of length 2 or greater
+#' @export
 carry_forward <-
         function(vector) {
                 for (i in 2:length(vector)) {
                         if (is.na(vector[i])) {
-                                value[i] <- value[(i-1)]
+                                vector[i] <- vector[i-1]
                         }
+                }
+                return(vector)
         }
-}
