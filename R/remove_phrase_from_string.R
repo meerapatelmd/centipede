a0cadb8f100615bce2bd62921ab84464037e5910
Meera Patel <patelm9@mskcc.org>
2019-10-05 15:10:40 -0400

    add function

---
+++ R/remove_phrase_from_string.R
@@ -0,0 +1,11 @@
+#' Removes a pharse from a string
+#' @param string character vector of length 1
+#' @param phrase character vector of length 1 to be removed from string
+#' @importFrom stringr str_replace_all
+#' @export
+#' 
+remove_phrase_from_string <-
+        function(string, phrase) {
+                p <- paste0("(.*\\s{0,1})(", phrase, ")(\\s{0,1}.*)")
+                stringr::str_replace_all(gsub(p, paste0("\\1 \\3"), string), " {2,}", " ")
+        }
