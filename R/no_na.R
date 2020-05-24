e1046097366cf238ba9348a2ed2f2bd4ba032871
Meera Patel <patelm9@mskcc.org>
2020-05-24 13:52:47 -0400

    add incl_str parameter to no_na function

--- R/no_na.R
+++ R/no_na.R
@@ -1,7 +1,15 @@
 #' Remove NA from a string
+#' @param incl_str Include "NA" in the removal?
 #' @export
 
 no_na <-
-        function(vector) {
+        function(vector, incl_str = TRUE) {
+                output <-
                 vector[!(is.na(vector))]
+
+                if (incl_str) {
+                       output <- output[!(output %in% c("NA"))]
+                }
+
+                return(output)
         }
