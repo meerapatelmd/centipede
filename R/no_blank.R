190a998d1cc065c6cfe3ffeac9ee9e4e95c42e18
Meera Patel <patelm9@mskcc.org>
2020-05-30 16:56:51 -0400

    add all_lengths option to no_blank function

--- R/no_blank.R
+++ R/no_blank.R
@@ -1,7 +1,16 @@
 #' Remove blank from a string
+#' @param all_lengths if TRUE, strings of one space (ie "^ $") and longer (ie "^          $") are also removed.
 #' @export
 
 no_blank <-
-        function(vector) {
-                vector[!(vector %in% "")]
+        function(vector, all_lengths = FALSE) {
+                if (all_lengths) {
+
+                        grep(pattern = "^[ ]{1,}$",
+                             vector,
+                             invert = TRUE)
+
+                } else {
+                        vector[!(vector %in% "")]
+                }
         }
